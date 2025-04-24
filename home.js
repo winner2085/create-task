0fefb9bb2157c570e89768b5ac0afcdeb236c971
Erica <ericac2700@tuckahoeschools.org>
2025-04-24 18:53:30 +0000

    f

--- home.js
+++ home.js
@@ -1,47 +1,48 @@
+if(page.title === "Home Page"){
+    //Creates buttons to add new inventory folders and items
+    const newInvBtn = document.getElementById("btn");
 
-//Creates buttons to add new inventory folders and items
-const newInv = document.querySelector("#btn");
+    //When button is clicked, it creates a new inventory folder to personalize
+    function createInv() {
+        newInvBtn.addEventListener('click', () => {
+            document.getElementById("new-inv").style.display = "block";
+        })
+    }
 
-//When button is clicked, it creates a new inventory folder to personalize
-function show(newInv) {
-    newInv.addEventListener('click', () => {
-        document.getElementById("new-inv").style.display = "block";
-    })
+    let inputTitle = document.getElementById("inTit").value;
+    let inputDesc = document.getElementById("inDesc").value;
+    const closeBtn = document.getElementById("closeBtn");
+
+    function storeUserInput(inputTitle, inputDesc){
+        closeBtn.addEventListener('click', () => {
+            const userInput = {
+                title: inputTitle,
+                description: inputDesc
+            }
+            console.log(userInput)
+            localStorage.setItem("UserInput", JSON.stringify(userInput));
+            window.location.href = "inv.html";
+        });
+    }
+    storeUserInput(inputTitle, inputDesc);
+}  else if (page.title === "Your Inventories") {
+    //Creates buttons to add new inventory items
+
+    const storedInput = localStorage.getItem("UserInput");
+    if (storedInput){
+        const userInput = JSON.parse(storedInput);
+
+        document.getElementById("invTitle").textContent = userInput.title;
+        document.getElementById("invDesc").textContent = userInput.description;
+    }
+
+    const addButton = document.getElementById("add-button");
+
+    //When button is clicked, it creates a new item to personalize
+    function addItem() {
+        addButton.addEventListener ('click', () => {
+            document.getElementById("container").style.display = "block";
+        });
+    }   
+    addItem();
 }
-show(newInv);
-
-let inputTitle = document.getElementById("input-title").value;
-let inputDesc = document.getElementById("input-desc").value;
-const closeBtn = document.getElementById("closeBtn");
-
-function storeUserInput(inputTitle, inputDesc){
-    closeBtn.addEventListener('click', () => {
-        const userInput = {
-            title: inputTitle,
-            description: inputDesc
-        }
-        console.log(userInput)
-        localStorage.setItem("UserInput", JSON.stringify(userInput));
-        window.location.href = "inv.html";
-    });
-}
-storeUserInput(inputTitle, inputDesc);
-
-const storedInput = localStorage.getItem("UserInput");
-
-if (storedInput){
-    const userInput = JSON.parse(storedInput);
-
-    document.getElementById("invTitle").textContent = userInput.title;
-    document.getElementById("invDesc").textContent = userInput.description;
-}
-
-const addButton = document.getElementById("add-button");
-
-//When button is clicked, it creates a new item to personalize
-function addItem() {
-    addButton.addEventListener ('click', () => {
-        document.getElementById("container").style.display = "block";
-    });
-}
-addItem();
